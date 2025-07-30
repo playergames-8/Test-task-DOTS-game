3b26fdce18871de774765a2725ef767afd87d209
playergames-8 <wera.sobczak8@gmail.com>
2025-07-30 03:46:42 +0200

    Added basic server logic
    Spent over 2h debbuging coz I used .withnone instead of .withall
    *screaming*
    anyhow, it has client and host in playmode and RPC for Ingame status works on both sides

---
+++ DOTS game/Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.NetCode;
+
+[UnityEngine.Scripting.Preserve]
+public class GameBootstrap : ClientServerBootstrap
+{
+    public override bool Initialize(string defaultWorldName)
+    {
+        AutoConnectPort = 7979;
+        return base.Initialize(defaultWorldName);
+    }
+}
